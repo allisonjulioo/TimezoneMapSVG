9f74d59f7a0e336d041ec62cdaee9165ac4e3067
Allison Nunes <allison.julio@hotmail.com>
2023-05-22 00:05:38 -0300

    test: add store test timezone

---
+++ src/store/__tests__/useTimezone.spec.ts
@@ -0,0 +1,13 @@
+import { useTimezone } from "../useTimezone";
+
+describe("Timezone Store", () => {
+  it("should correct set timezone offset store", () => {
+    const timerStore = useTimezone();
+
+    expect(timerStore.ofsetTimezone).toBe('0');
+
+    timerStore.setOfsetTimezone('2');
+
+    expect(timerStore.ofsetTimezone).toBe('2');
+  });
+});
